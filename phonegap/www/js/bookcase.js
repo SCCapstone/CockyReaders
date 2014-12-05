48c62c4a87e33122ab7ca9c1d3d0c104de782323
bainco <bainco@email.sc.edu>
2014-12-05 00:42:29 -0500

    Fixed bookcase display issue

--- phonegap/www/js/bookcase.js
+++ phonegap/www/js/bookcase.js
@@ -15,7 +15,8 @@ function updateBookcase() {
 		theBook = bookList[i];
 		
         var span = document.createElement("span");
-		span.style.padding = "8px 0px 36px 100px";
+        span.style.float = "left"
+		span.style.padding = "8px 0px 36px 135px";
 		span.style.height = "150px";
 		span.style.display = "block";
 			
@@ -26,7 +27,8 @@ function updateBookcase() {
 		a.href = "#";
 			
 		var div2 = document.createElement("div");
-		div2.class = "caption-wrapper";
+		div2.className = "caption-wrapper";
+        div2.style.fontSize = ".1px"
 		div2.style.overflow = "hidden";
 		div2.style.padding = "0px";
 		div2.style.width = "111px";
@@ -40,23 +42,48 @@ function updateBookcase() {
              };
 		image.src = theBook.cover;
 		image.rel = "book_thumb_8";
-		image.class = " captify book_thumb";
+		image.className = " captify book_thumb";
 		image.style.border = "0px none";
 		image.style.margin = "0px";
         
         var a2 = document.createElement("a");
         a2.href = "#"
-        a2.class = "thumblink"
+        a2.className = "thumblink"
         a2.style.margin = "0px"
+        
+        div3 = document.createElement("div")
+        div3.className = "caption-bottom"
+        div3.style.margin="0px"
+        div3.style.zIndex="1"
+        div3.style.position = "relative"
+        div3.style.opacity = "0.7"
+        div3.style.width = "116px"
+        div3.style.height = "38.40625px"
+        
+        div4 = document.createElement("div");
+        div4.style.margin = "-52px 0px 0px"
+        div4.className = "caption-bottom"
+        div4.style.paddingTop = "9x"
+        div4.style.position = "relative"
+        div4.style.zIndex = "2"
+        div4.style.backgroundImage = "none"
+        div4.style.border = "0px none"
+        div4.style.opacity = "1"
+        div4.style.width = "100%"
+        div4.style.backgroundPosition = "initial initial"
+        div4.style.backgroundRepeat = "initial initial"
         	
 		div2.appendChild(image);
-        div2.appendChild(a2);
+        div2.appendChild(div3);
+        
+        div4.appendChild(a2)
+        div2.appendChild(div4);
         
 		a.appendChild(div2);
 		div1.appendChild(a);
 		span.appendChild(div1);	
 		i++;	
-        bookshelfDiv.appendChild(span);	
+        bookshelfDiv.appendChild(span);
 	}
 }
 
