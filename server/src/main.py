8bbcd11f2cab986958beec86b9f27ed63fc822e9
mitchem <andrewmitchem70@yahoo.com>
2015-02-21 12:38:20 -0500

    Update main.py

--- server/src/main.py
+++ server/src/main.py
@@ -53,26 +53,23 @@ class MainPage(webapp2.RequestHandler):
 #class for login side of the app        
 class LoginHandler(MainPage):
     def get(self):
-        if(self.request.get('logintype') == google):
-            self.setupUser()
-        if(self.request.get('logintype') == ourdata):
-            self.loginUser = self.request.get('user')
-            self.loginPassword = self.request.get('password')
+        self.loginUser = self.request.get('user')
+        self.loginPassword = self.request.get('password')
             
-            #demo user
-            query = Student.all()
-            if query.count() == 0:
-                newStudent = Student(firstname="temp",lastname="temp",username="theFirst",password="password, books= [1113,1113])
-                newStudent.put()
+        #demo user
+        query = Student.all()
+        if query.count() == 0:
+            newStudent = Student(firstname="temp",lastname="temp",username="theFirst",password="password, books= [1113,1113])
+            newStudent.put()
                 
-            key = db.key.from_path('Student', loginUser )
-            theStudent = db.get(key)
-            if theStudent == None:
-                self.redirct('/')
-            if theStudent.password != loginPassword:
-                self.redirct('/')
-            #code for return a successful login 
-            #
+        key = db.key.from_path('Student', loginUser )
+        theStudent = db.get(key)
+        if theStudent == None:
+            self.redirct('/')
+        if theStudent.password != loginPassword:
+            self.redirct('/')
+        #code for return a successful login 
+        #
     def post(self):
         #for setting up users
 class BookHandler(MainPage):
