a2539936d0277a5ed335b79fc0d668b2a7c0b38e
bainco <bainco@email.sc.edu>
2015-03-01 14:06:29 -0500

    Fixed merge issues.
    I spent over 3 hours trying to fix all of the mistakes. Some were small, some were large. In addition to taking over the responsiblities of the local portion since one of our members is MIA, I also had to correct all of the issues with the Interface and Server codes. To be honest, I'm not sure if either were tested in-browser. I ran into several errors from code that would just not compile.

--- phonegap/www/js/login.js
+++ phonegap/www/js/login.js
@@ -1,23 +1,57 @@
 function GET_Login(theForm) {
     
-     outAJAX++;
-    $.ajax('http://localhost:8080/Login', {
+    outAJAX++;
+    var theForm = document.getElementById("loginForm");
+    
+    data = {"user": theForm.user.value,
+            "password": theForm.password.value}
+    
+   $.ajax('http://localhost:9080/login', {
         type: 'GET',
-        data: {
-            user:theForm.username;
-            pass:theForm.password;
-        },
+        data: data,
+        dataType: 'json',
         success: function(data){			
             console.log('Login successful!');
-            setCookie("user", theForm.username, 1);
-            //REDIRECT
+            setCookie("user", data, 1);
+            location.href = "bookshelf.html"
         },
-        error: function() {
+        error: function(jqXHR, textStatus, errorThrown) {
             console.log('Login failed!');
-            //ALERT: Please register
+            alert("Login failed! Please try again!")
+            console.log(errorThrown);
         },
         complete: checkAJAX
     });
+    return false;
+}
+
+function POST_Login() {
+    outAJAX++;
+
+    var theForm = document.getElementById("registerForm");
+    
+    data = {"user": theForm.user.value,
+            "password": theForm.password.value, 
+            "firstName": theForm.firstName.value,
+            "lastName": theForm.lastName.value,
+            "teacher": theForm.teacher.value,
+            "grade": theForm.grade.value,
+            "pinNumber": theForm.pinNumber.value}
+    
+   $.ajax('http://localhost:9080/login', {
+        type: 'POST',
+        data: data,
+        dataType: 'text',
+        success: function(){			
+            console.log('Registration successful!');
+            location.href = "index.html";
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('Registration failed!');
+        },
+        complete: checkAJAX
+    });
+    return false;
 }
 
 // Function to set a cookie with name 'cname', value 'cvalue', and expiration time 'exdays'
@@ -25,17 +59,29 @@ function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/;";
+    document.cookie = cname + "=" + cvalue + ", " + expires + ", path=/;";
+}
+
+// Function to get the username from a cookie
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
 }
 
 // Method to check if other outgoing ajax requests
 function checkAJAX() {
     //console.log("Ajax request complete.")
     outAJAX = outAJAX - 1;
-    if (outAJAX == 0) updateView();
 }
 
 var outAJAX = 0;
+
 $(document).ready(function() {
     console.log("ready");
 });
